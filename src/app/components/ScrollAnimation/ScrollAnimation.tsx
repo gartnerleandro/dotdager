378b80178ee2dc0dbe372a3d1c3fecd304f1eaf5
gartnerleandro <gartnerleandro@gmail.com>
2024-11-22 10:31:22 +0100

    feat: add new ScrollAnimation component

---
+++ src/app/components/ScrollAnimation/ScrollAnimation.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React, { useEffect } from "react";
+
+import LottieComponent from "../Lottie/Lottie";
+
+import "./ScrollAnimation.css";
+
+export default function ScrollAnimation() {
+    const [withScroll, setWithScroll] = React.useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          if (window.scrollY > 280) {
+            setWithScroll(true);
+          } else {
+            setWithScroll(false);
+          }
+        };
+
+        handleScroll();
+    
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
+
+    
+
+    return (withScroll ? null :
+      <button className="scroll-bottom" onClick={() => scrollTo({ top: 400, behavior: "smooth" })}>
+        <LottieComponent
+              animationData={require("@/../public/scroll.json")}
+              className="lottie-component"
+          />
+      </button>
+        
+    );
+}
