0238e7daffe8ebc05f675d0a2fd6fd40a577bd38
gartnerleandro <gartnerleandro@gmail.com>
2024-11-22 10:34:38 +0100

    feat: add RootLayout component

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import { ThemeProvider } from "next-themes";
+
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 400 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 400 900",
+});
+
+export const metadata: Metadata = {
+  title: "DotDager",
+  description: "Full-stack senior y contador serial de chistes de mierda",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ThemeProvider attribute="class">
+          {children}
+          <ScrollToTop />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
