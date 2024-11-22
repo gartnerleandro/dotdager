5ee86c09683f50e43a5411c802f8921e26b0c090
gartnerleandro <gartnerleandro@gmail.com>
2024-11-22 10:45:23 +0100

    refactor: update ThemeSwiutcher component to use the pointer glow custom hook

--- src/app/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/app/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -3,11 +3,15 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
+
+import { usePointerGlow } from '../../hooks/usePointerGlow/usePointerGlow';
+
 import "./ThemeSwitcher.css";
 
 const ThemeSwitcher = () => {
     const [mounted, setMounted] = useState(false);
     const { theme, setTheme } = useTheme();
+    usePointerGlow();
     
     useEffect(() => setMounted(true), []);
     
