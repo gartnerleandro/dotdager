6d48b748d9fbd55c80e8a18721d590fcbd99a867
gartnerleandro <gartnerleandro@gmail.com>
2024-11-22 11:37:24 +0100

    refactor: update project route structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,17 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { ThemeProvider } from "next-themes";
 
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 import "./globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 400 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 400 900",
 });
