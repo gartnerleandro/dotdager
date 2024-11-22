1bbd5b02502006d4b11caa8a74434a6d4207fed5
gartnerleandro <gartnerleandro@gmail.com>
2024-11-22 10:44:25 +0100

    feat: add new custom hook  usePointerGlow

---
+++ src/app/hooks/usePointerGlow/usePointerGlow.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from 'react';
+
+export const usePointerGlow = () => {
+  const [status, setStatus] = React.useState(null);
+  React.useEffect(() => {
+    function syncPointer({ x: pointerX, y: pointerY }: { x: number, y: number }): void {
+      const x = pointerX.toFixed(2);
+      const y = pointerY.toFixed(2);
+      const xp = (pointerX / window.innerWidth).toFixed(2);
+      const yp = (pointerY / window.innerHeight).toFixed(2);
+      document.documentElement.style.setProperty('--x', x);
+      document.documentElement.style.setProperty('--xp', xp);
+      document.documentElement.style.setProperty('--y', y);
+      document.documentElement.style.setProperty('--yp', yp);
+      // @ts-ignore
+      setStatus({ x, y, xp, yp });
+    }
+    document.body.addEventListener('pointermove', syncPointer);
+    return () => {
+      document.body.removeEventListener('pointermove', syncPointer);
+    };
+  }, []);
+  return [status];
+};
