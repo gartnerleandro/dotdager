9d7d37305ea64fc16e52459d2b7839ac7d240f30
gartnerleandro <lean.argentino@hotmail.com>
2024-11-23 13:14:07 +0100

    test: cleaning layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import { ThemeProvider } from "next-themes";
-
-import ScrollToTop from "@/app/_components/ScrollToTop/ScrollToTop";
 
 import "./globals.css";
 
@@ -30,10 +27,7 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProvider attribute="class">
           {children}
-          <ScrollToTop />
-        </ThemeProvider>
       </body>
     </html>
   );
