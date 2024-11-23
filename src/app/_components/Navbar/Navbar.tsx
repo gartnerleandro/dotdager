6b21cfc9930655a1114fd71f8c495b47ac08966a
gartnerleandro <lean.argentino@hotmail.com>
2024-11-23 11:18:43 +0100

    feat: add navbar

---
+++ src/app/_components/Navbar/Navbar.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useRef, useState } from 'react';
+import Lottie from "lottie-react";
+import animationData from '@/../public/menu.json';
+
+import './Navbar.css';
+
+const Navbar = () => {
+  const menuButton = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    
+    if (menuButton?.current) {
+        if (isOpen) {
+            //@ts-expect-error
+            menuButton.current?.setSpeed(3);
+            //@ts-expect-error
+            menuButton.current?.playSegments([85, 140]);
+        } else {
+            //@ts-expect-error
+            menuButton.current?.setSpeed(3);
+            //@ts-expect-error
+            menuButton.current?.playSegments([0, 85]);
+        }
+    }
+  };
+
+  const scrollToSection = (sectionId: string) => {
+    toggleMenu();
+    const element = document.getElementById(sectionId);
+    element?.scrollIntoView({ behavior: 'smooth' });
+  };
+
+  return (
+    <nav className={`navbar ${isOpen ? 'active' : ''}`}>
+      <div className="backdrop" onClick={toggleMenu}></div>
+      <button className={`hamburger ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+        <Lottie
+            loop={false}
+            autoPlay={false}
+            initialSegment={[0, 1]}
+            lottieRef={menuButton}
+            animationData={animationData}
+            className={isOpen ? "open": "closed"}
+        />
+      </button>
+
+      <div className={`nav-menu ${isOpen ? 'active' : ''}`}>
+        <ul>
+          <li onClick={() => scrollToSection('projects')}>Proyectos</li>
+          <li onClick={() => scrollToSection('quotes')}>Citas</li>
+          <li onClick={() => scrollToSection('about')}>Sobre mí</li>
+          <li onClick={() => scrollToSection('contact')}>Contacto</li>
+        </ul>
+      </div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
