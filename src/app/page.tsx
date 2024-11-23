d6b5eaeccecff6adcc619d6d73838431975f9ac6
gartnerleandro <lean.argentino@hotmail.com>
2024-11-23 13:26:22 +0100

    test: deploy without client components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
 import { Axe, Linkedin, Youtube, Github, TreePalm, Instagram } from 'lucide-react';
 
-import Typewriter from '@/app/_components/Typewriter/Typewriter';
-import ScrollAnimation from '@/app/_components/ScrollAnimation/ScrollAnimation';
-import ThemeSwitcher from '@/app/_components/ThemeSwitcher/ThemeSwitcher';
-import Navbar from '@/app/_components/Navbar/Navbar';
-
 import "./page.css";
 
 export default function Home() {
   return (
     <div className="page">
       <main className="main">
-        <ThemeSwitcher />
-        <Navbar />
+
         <section className="hero">
           <div className="presentation">
             <div className="profile-wrapper" data-glow>
@@ -20,19 +14,69 @@ export default function Home() {
             </div>
             
             <h2>Hola, soy Dot Dager</h2>
-            <Typewriter
-              strings={["Full-Stack/Lead Developer.", "Creador de contenido.", "Amante de la filosofía.", "Guitarrista.", "Catador de pepinos.", "Campeón del lumberjack."]}
-              autoStart={true}
-              loop={true}
-            />
+            
             <a data-glow title="lumberjack" href="https://www.tbot.xyz/lumber/" target="_blank" className="fake-cv">
               <Axe />
               Consultar CV
             </a>
           </div>
           
-          <ScrollAnimation />
           
+          
+        </section>
+
+        <section id="projects">
+          <h2>Proyectos</h2>
+
+          <div className="projects-list">
+            
+          </div>
+
+          <h3 className="technologies-title">Tecnologías utilizadas</h3>
+          <ul className="technologies">
+            <li data-glow>
+              <img loading="lazy" src="/net.webp" width="128" height="128" alt=".net" />
+            </li>
+            <li data-glow>
+              <img loading="lazy" src="/aws.webp" width="128" height="128" alt="aws" />
+            </li>
+            <li data-glow>
+              <img loading="lazy" src="/java.webp" width="128" height="128" alt="java" />
+            </li>
+            <li data-glow>
+              <img loading="lazy" src="/mysql.webp" width="128" height="128" alt="mysql" />
+            </li>
+            <li data-glow>
+              <img loading="lazy" src="/swift.webp" width="128" height="128" alt="swift" />
+            </li>
+            <li data-glow>
+              <img loading="lazy" src="/winforms.webp" width="128" height="128" alt="winforms" />
+            </li>
+            <li data-glow>
+              <img loading="lazy" src="/xamarin.webp" width="128" height="128" alt="xamarin" />
+            </li>
+          </ul>
+        </section>
+
+        <section id="quotes">
+          
+        </section>
+
+        <section id="about">
+          <h2>Sobre mí</h2>
+          <p>
+            Soy un desarrollador de software con más de 19 años de experiencia en la creación de aplicaciones web y móviles. Esta es mi trayectoria profesional:
+          </p>
+          <br />
+          
+        </section>
+
+        <section id="contact">
+          <h2>Contacto</h2>
+          <div>
+            <img loading="lazy" src="/contacto.webp" width="300" height="400" alt="contactame" />
+            
+          </div>
         </section>
       </main>
       <footer>
